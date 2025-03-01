02d9b190026e218e57c3fb0afeada473d5bb3206
ccrawford4 <calumcrawford9@gmail.com>
2025-03-01 07:06:29 -0800

    working select and create with semantic relese

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
-
 export type Restaurant = {
     ID: string
     name: string
@@ -16,7 +14,6 @@ export type RestaurantCreateInput = {
 }
 
 export const getRestaurants = async (): Promise<Restaurant[]> => {
-    console.log("API_URL: ", API_URL);
     const response = await axios.get(`/api/restaurants`);
     return response.data;
 };
@@ -32,9 +29,9 @@ export const addRestaurant = async (data: RestaurantCreateInput): Promise<Restau
 };
 
 export const updateRestaurant = async (data: { data: Omit<Restaurant, "ID">; ID: any }) => {
-    await axios.put(`${API_URL}/api/restaurants/${data.ID}`, data.data);
+    await axios.put(`/api/restaurants/${data.ID}`, data.data);
 };
 
 export const deleteRestaurant = async (id: string) => {
-    await axios.delete(`${API_URL}/api/restaurants/${id}`);
+    await axios.delete(`/api/restaurants/${id}`);
 };
